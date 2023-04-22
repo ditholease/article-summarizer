d82d25206bd263c150798ddd66cd24b8893820fa
Christos Stefanis <xristostefanis@gmail.com>
2023-04-22 12:26:24 +0300

    Bring tailwindcss, fonts & assets to the project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        satoshi: ['Satoshi', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
